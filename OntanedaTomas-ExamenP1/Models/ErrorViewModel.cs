fcbf1eb586df7e0e9ac07624d087b7637171b4c1
tom-ayu <tomas.ontaneda@udla.edu.ec>
2024-10-28 07:17:23 -0500

    Agregar archivos de proyecto.

---
+++ OntanedaTomas-ExamenP1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OntanedaTomas_ExamenP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
