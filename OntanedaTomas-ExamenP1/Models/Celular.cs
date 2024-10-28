f08e0da303f034fa2e6efd83c65d7372c7886f07
tom-ayu <tomas.ontaneda@udla.edu.ec>
2024-10-28 07:38:19 -0500

    Modelos definidos

---
+++ OntanedaTomas-ExamenP1/Models/Celular.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace OntanedaTomas_ExamenP1.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Modelo { get; set; }
+        [Range(1980, 2024)]
+        public int Anio { get; set; }
+        [Required]
+        [AllowNull]
+        public float Precio { get; set; }
+    }
+}
---
+++ OntanedaTomas-ExamenP1/Models/TOntaneda.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OntanedaTomas_ExamenP1.Models
+{
+    public class TOntaneda
+    {
+        [Key]
+        public int IdCPU {  get; set; }
+        [Range(1,5)]
+        public float VelocidadCPU { get; set; }
+        [MaxLength(100)]
+        public string Marca { get; set; }
+        [Required]
+        public bool EstaOverclokeado { get; set; }
+        public DateOnly FechaObtencion { get; set; }
+        public Celular Celular { get; set; }
+        [ForeignKey("Celular")]
+        public int IdCelular {  get; set; }
+
+    }
+}
